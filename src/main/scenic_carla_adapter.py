0cc31d88a68df66b3de0922c27640eb75fcaa92b
严宋扬 <tayeep@dingtalk.com>
2021-03-19 14:36:15 +0800

    add scenic parser and doc

--- src/main/scenic_carla_adapter.py
+++ src/main/scenic_carla_adapter.py
@@ -3,24 +3,24 @@ from src.main.carla_adapter import CarlaAdapter
 import scenic
 from src.tools import utils
 
+
 class ScenicCarlaAdapter(CarlaAdapter):
-    def __init__(self, ip_address, carla_map):
+    def __init__(self, ip_address):
         super().__init__(ip_address)
-        self.params = {}
-        self.params["address"] = ip_address
-        # TODO: remove
-        self.params["carla_map"] = carla_map
+        # self.params = {}
+        # self.params["address"] = ip_address
+        # # TODO: remove
+        # self.params["carla_map"] = carla_map
         self.scene = None
         self.simulator = None
         self.simulate_thread = None
 
-    def set_map(self, map):
-        super().set_map(map)
-        self.params["carla_map"] = map
+    # def set_map(self, map):
+    #     super().set_map(map)
+        # self.params["carla_map"] = map
 
-    def init(self, code_file):
-        scenario = scenic.scenarioFromFile(path = code_file, model="scenic.simulators.carla.model", params=self.params)
-        self.scene,_ = scenario.generate()
+    def init(self, scenario):
+        self.scene, _ = scenario.generate()
         self.simulator = scenario.getSimulator()
         self.simulator.render = False
 
@@ -32,7 +32,6 @@ class ScenicCarlaAdapter(CarlaAdapter):
                 self.scene.objects = tuple(temp_list)
                 return car_object
         return None
-
 
     def run(self):
         self.simulate_thread = SimulateThread(self.simulator, self.scene)
@@ -50,6 +49,7 @@ class ScenicCarlaAdapter(CarlaAdapter):
         finally:
             self.simulate_thread = None
 
+
 class SimulateThread(threading.Thread):
     def __init__(self, simulator, scene):
         threading.Thread.__init__(self)
@@ -60,7 +60,7 @@ class SimulateThread(threading.Thread):
         try:
             self.simulator.simulate(self.scene, verbosity=0)
         except Exception as exception:
-            print("Scenic error:{}".format(exception)) 
+            print("Scenic error:{}".format(exception))
 
     def stop(self):
         try:
---
+++ src/scenic_parser/inputs/follow.scenic
@@ -0,0 +1,59 @@
+""" Scenario Description
+The ego vehicle follows the lead car which suddenly stops
+The av_ego follows the lane
+"""
+
+#SET MAP AND MODEL (i.e. definitions of all referenceable vehicle types, road library, etc)
+param map = localPath('../third-party/scenic/CARLA/Town03.xodr') 
+param carla_map = 'Town03'
+model scenic.domains.driving.model
+
+#CONSTANTS
+MAX_BREAK_THRESHOLD = 1
+SAFETY_DISTANCE = 10
+INITIAL_DISTANCE_APART = -1*Uniform(5, 10)
+STEPS_PER_SEC = 10
+
+##DEFINING BEHAVIORS
+behavior LeadCarBehavior():
+	try:
+		do FollowLaneBehavior()
+	interrupt when simulation().currentTime > 100 * STEPS_PER_SEC:
+		take SetBrakeAction(MAX_BREAK_THRESHOLD)
+
+
+behavior CollisionAvoidance():
+	while withinDistanceToAnyObjs(self, SAFETY_DISTANCE):
+		take SetBrakeAction(MAX_BREAK_THRESHOLD)
+
+
+behavior FollowLeadCarBehavior():
+	try: 
+		do FollowLaneBehavior()
+
+	interrupt when withinDistanceToAnyObjs(self, SAFETY_DISTANCE):
+		do CollisionAvoidance()
+
+
+##DEFINING SPATIAL RELATIONS
+# Please refer to scenic/domains/driving/roads.py how to access detailed road infrastructure
+# 'network' is the 'class Network' object in roads.py
+
+roads = network.roads
+
+# make sure to put '*' to uniformly randomly select from all elements of the list, 'network.roads'
+select_road = Uniform(*roads)
+select_lane = Uniform(*select_road.lanes)
+lane = Uniform(*network.lanes)
+
+other = Car on select_lane.centerline,
+		with behavior LeadCarBehavior()
+
+ego = Car following roadDirection from other for INITIAL_DISTANCE_APART,
+		with behavior FollowLeadCarBehavior()
+
+start = OrientedPoint on lane.centerline
+
+av_ego = Car at start,
+		with rolename "AV_EGO",
+		with behavior FollowLaneBehavior(10)
