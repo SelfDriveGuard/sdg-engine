b4a64a666a37d7c6402b2ffa77a21d09fa87f556
严宋扬 <tayeep@dingtalk.com>
2021-04-12 15:52:07 +0800

    fuzz api 1

--- src/tools/utils.py
+++ src/tools/utils.py
@@ -88,3 +88,26 @@ def get_assertion_timestamp(assertion_list):
         if assertion_list[i][1] == False:
             return assertion_list[i][0]   
     return None
+
+# 按一定时间间隔重复执行任务
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
